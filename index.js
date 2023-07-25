014182c436e94a7d3944fc3da671125825a38a50
Neha kumari <nehakumari@Nehas-MacBook-Pro.local>
2023-07-25 21:13:54 +0530

    database connected

---
+++ index.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb');
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://Test123:Test123@cluster0.6qidosj.mongodb.net/Test_Users?retryWrites=true&w=majority";
+
+
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        console.log('connection successfully')
+        // Make the appropriate DB calls
+        // await  listDatabases(client);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
